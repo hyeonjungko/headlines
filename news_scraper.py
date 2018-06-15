542dbf2a2a9512da94fbd5aa53918f07be5da005
Hyeonjung Ko <hjko1203@gmail.com>
2018-06-14 23:09:12 -0400

    changed some formatting

--- news_scraper.py
+++ news_scraper.py
@@ -1,14 +1,25 @@
 #!/usr/local/bin/python3
-
 import requests
 
-def main(): 
-	#sources = ['the-wall-street-journal']
-	url = ('https://newsapi.org/v2/top-headlines?'
-		'sources=the-wall-street-journal&'
-		'apiKey=6852f94cb1ed44f98e6845a2b21c28c9')
-	response = requests.get(url)
-	print(response.json())
+
+def display(data):
+    #print(data['articles'])
+
+    for article in data['articles']:
+        print(article['title'])
+        print(article['url'])
+        print(article['description'])
+        print('')
+
+def get_articles(): 
+    url = ('https://newsapi.org/v2/top-headlines?'
+            'sources=the-wall-street-journal&'
+            'apiKey=6852f94cb1ed44f98e6845a2b21c28c9')
+    response = requests.get(url)
+    data = response.json()
+    display(data)
 
 if __name__=='__main__':
-	main()
+    #sources = ['the-wall-street-journal&', ...]
+    #get_article(sources)
+    get_articles()
